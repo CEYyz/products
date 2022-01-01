50dcd6cb5b344c25ab12c29bd37c6971eb0d495d
Edison <edison448@gmail.com>
2022-01-01 21:45:45 +0800

    add os function

--- products.py
+++ products.py
@@ -1,15 +1,19 @@
-#讀取檔案
+import os # oprating system
 products =[]
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品, 價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+# 讀取檔案
+if os.path.isfile('products.csv'):#檢查檔案
+	print('find this file')
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品, 價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('fail...')
 
-
-#使用者輸入
+# 使用者輸入
 products = []
 while True:
 	name = input('pls type product name: ')
@@ -23,10 +27,11 @@ while True:
 	#p.append(price)
 	products.append(p)
 
+# 購買紀錄
 for pro in products:
 	print(pro[0], '的價格是', pro[1])
 
-#寫入檔案
+# 寫入檔案
 with open('products.csv', 'w') as f:
 	f.write('商品, 價格\n')
 	for p in products:
