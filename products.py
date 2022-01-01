c204b98d87e9581d534b0ff698c856a244550e4d
Edison <edison448@gmail.com>
2022-01-01 20:40:15 +0800

    upload latest version

--- products.py
+++ products.py
@@ -13,6 +13,6 @@ while True:
 for pro in products:
 	print(pro[0], '的價格是', pro[1])
 
-#with open('products.txt', 'w') as f:
-#	for p in products:
-#		f.write(p[0] + ',' +p[1] + '\n')
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' +p[1] + '\n')
