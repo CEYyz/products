1f3b9b68df86e69e0c70a1000c4099427db4f801
Edison <edison448@gmail.com>
2022-01-01 21:30:34 +0800

    add new function read

--- products.py
+++ products.py
@@ -1,3 +1,15 @@
+#讀取檔案
+products =[]
+with open('products.csv', 'r') as f:
+	for line in f:
+		if '商品, 價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+
+
 products = []
 while True:
 	name = input('pls type product name: ')
