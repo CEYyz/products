3434043a668067cd58d48795368ebb257a116ebf
Edison <edison448@gmail.com>
2022-01-01 21:32:08 +0800

    add descroption

--- products.py
+++ products.py
@@ -9,7 +9,7 @@ with open('products.csv', 'r') as f:
 print(products)
 
 
-
+#使用者輸入
 products = []
 while True:
 	name = input('pls type product name: ')
@@ -26,6 +26,7 @@ while True:
 for pro in products:
 	print(pro[0], '的價格是', pro[1])
 
+#寫入檔案
 with open('products.csv', 'w') as f:
 	f.write('商品, 價格\n')
 	for p in products:
