b97c935988f78a2d8f2d8545b9b59b8ddf97bcc1
Edison <edison448@gmail.com>
2022-01-01 21:13:37 +0800

    add feader

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('pls type product price: ')
+	price = int(price)
 	p = [name, price]
 	#p = []
 	#p.append(name)
@@ -14,5 +15,6 @@ for pro in products:
 	print(pro[0], '的價格是', pro[1])
 
 with open('products.csv', 'w') as f:
+	f.write('商品, 價格\n')
 	for p in products:
-		f.write(p[0] + ',' +p[1] + '\n')
+		f.write(p[0] + ',' +str(p[1]) + '\n')
